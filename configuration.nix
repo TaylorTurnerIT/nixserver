9ba5b2f8694386423ea5da664f930e70602bbded
Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>
2025-11-25 20:13:37 -0600

    Update root user's SSH authorized keys in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -27,12 +27,12 @@
 
   # users.users.nixos.openssh.authorizedKeys.keys = [
   #   # Public Keys default nixos user
-  #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBbBcwPIsJV1MBuNS6V9Agwbc5hYc0Ojgi7UA3EXbAD taylorturnerit@gmail.com" 
+  #   "" 
   # ];
 
   users.users.root.openssh.authorizedKeys.keys =  [
     # Public Keys for root user
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBbBcwPIsJV1MBuNS6V9Agwbc5hYc0Ojgi7UA3EXbAD taylorturnerit@gmail.com" 
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJB9MG22hSHdYpwIWFRanUF88YvOYNcrV1zxAvv2RDJt taylort3450@syn-2600-6c5d-567f-3f2b-c338-35e0-ec14-df45.biz6.spectrum.com" 
   ];
 
   # --- PODMAN ---
