3f6451ed2a3225f7e14e06c418f5df8f815732e0
Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>
2025-12-15 14:57:24 -0600

    Add Jexactyl configuration: add database secrets, update .env template

--- configuration.nix
+++ configuration.nix
@@ -1,102 +1,131 @@
 { config, pkgs, modulesPath, ... }:
 
-{
-  imports = [ 
-    (modulesPath + "/profiles/qemu-guest.nix")
-    ./disko-config.nix
-    ./containers/default.nix
-    ./network/default.nix
-  ];
+	{
+	imports = [ 
+		(modulesPath + "/profiles/qemu-guest.nix")
+		./disko-config.nix
+		./containers/default.nix
+		./network/default.nix
+	];
 
-  # --- BOOTLOADER ---
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+	# --- BOOTLOADER ---
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "homelab";
-  networking.networkmanager.enable = true;
+	networking.hostName = "homelab";
+	networking.networkmanager.enable = true;
 
-  # --- PROXMOX INTEGRATION ---
-  services.qemuGuest.enable = true; #
-  boot.kernelModules = [ "kvm-intel" ]; 
-  
-  # --- SERVER HARDENING ---
-   services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "prohibit-password"; # Only keys allowed
-      PasswordAuthentication = false;        # Disable passwords completely
-      KbdInteractiveAuthentication = false;  # Disable keyboard-interactive auth
-    };
-  };
+	# --- PROXMOX INTEGRATION ---
+	services.qemuGuest.enable = true; #
+	boot.kernelModules = [ "kvm-intel" ]; 
 
-  # --- SOPS Config ---
-  sops.defaultSopsFile = ./secrets/secrets.yaml;
-  sops.defaultSopsFormat = "yaml";
-  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+	# --- SERVER HARDENING ---
+	services.openssh = {
+		enable = true;
+		settings = {
+		PermitRootLogin = "prohibit-password"; # Only keys allowed
+		PasswordAuthentication = false;        # Disable passwords completely
+		KbdInteractiveAuthentication = false;  # Disable keyboard-interactive auth
+		};
+	};
 
-  # --- SOPS Secrets ---
-  sops.secrets.cloudflare_token = {
-    owner = "acme";
-  };
-  sops.secrets.minecraft_rcon_password = {
-    # We leave the owner as root because Podman runs as root (for now)
-    owner = "root";
-  };
-  sops.secrets.foundry_admin_hash = {
-    owner = "root"; # Podman runs as root by default
-  };
-  # --- Secrets for Foundry VTT ---
-  sops.secrets.foundry_username = {};
-  sops.secrets.foundry_password = {};
-  sops.secrets.foundry_admin_pass = {};
+	# --- SOPS Config ---
+	sops.defaultSopsFile = ./secrets/secrets.yaml;
+	sops.defaultSopsFormat = "yaml";
+	sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
-  # Create the config.json template for the container
-  sops.templates."foundry_secrets.json" = {
-    content = ''
-      {
-        "foundry_username": "${config.sops.placeholder.foundry_username}",
-        "foundry_password": "${config.sops.placeholder.foundry_password}",
-        "foundry_admin_key": "${config.sops.placeholder.foundry_admin_pass}"
-      }
-    '';
-    mode = "0444";
-  };
+	# --- SOPS Secrets ---
+	sops.secrets.cloudflare_token = {
+		owner = "acme";
+	};
+	sops.secrets.minecraft_rcon_password = {
+		# We leave the owner as root because Podman runs as root (for now)
+		owner = "root";
+	};
+	sops.secrets.foundry_admin_hash = {
+		owner = "root"; # Podman runs as root by default
+	};
+	# --- Secrets for Foundry VTT ---
+	sops.secrets.foundry_username = {};
+	sops.secrets.foundry_password = {};
+	sops.secrets.foundry_admin_pass = {};
 
-  # --- Gitea Secrets ---
-  sops.secrets.gitea_secret_key = {
-    owner = "root"; # Podman runs as root
-  };
-  sops.secrets.github_mirror_token = {
-    owner = "root";
-  };
+	# Create the config.json template for the container
+	sops.templates."foundry_secrets.json" = {
+		content = ''
+		{
+			"foundry_username": "${config.sops.placeholder.foundry_username}",
+			"foundry_password": "${config.sops.placeholder.foundry_password}",
+			"foundry_admin_key": "${config.sops.placeholder.foundry_admin_pass}"
+		}
+		'';
+		mode = "0444";
+	};
 
-  # users.users.nixos.openssh.authorizedKeys.keys = [
-  #   # Public Keys default nixos user
-  # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJB9MG22hSHdYpwIWFRanUF88YvOYNcrV1zxAvv2RDJt taylort3450@syn-2600-6c5d-567f-3f2b-c338-35e0-ec14-df45.biz6.spectrum.com" 
-  # ];
+	# --- Gitea Secrets ---
+	sops.secrets.gitea_secret_key = {
+		owner = "root"; # Podman runs as root
+	};
+	sops.secrets.github_mirror_token = {
+		owner = "root";
+	};
 
-  users.users.root.openssh.authorizedKeys.keys =  [
-    # Public Keys for root user
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJB9MG22hSHdYpwIWFRanUF88YvOYNcrV1zxAvv2RDJt taylort3450@syn-2600-6c5d-567f-3f2b-c338-35e0-ec14-df45.biz6.spectrum.com" 
-  ];
+	# --- Jexactyl Secrets ---
+	sops.secrets.jexactyl_db_password = { };
+	sops.secrets.jexactyl_db_root_password = { };
+	sops.secrets.jexactyl_app_key = { };
 
-  # --- PODMAN ---
-  virtualisation.podman = {
-    enable = true;
-    dockerCompat = true;
-    defaultNetwork.settings.dns_enabled = true;
-  };
+	# Template to generate the .env file for the container
+	sops.templates."jexactyl.env" = {
+		content = ''
+		APP_ENV=production
+		APP_DEBUG=false
+		APP_KEY=${config.sops.placeholder.jexactyl_app_key}
+		APP_URL=https://panel.tongatime.us
+		
+		DB_HOST=jexactyl-mariadb
+		DB_PORT=3306
+		DB_DATABASE=panel
+		DB_USERNAME=jexactyl
+		DB_PASSWORD=${config.sops.placeholder.jexactyl_db_password}
+		
+		REDIS_HOST=jexactyl-redis
+		REDIS_PASSWORD=null
+		REDIS_PORT=6379
+		
+		CACHE_DRIVER=redis
+		SESSION_DRIVER=redis
+		QUEUE_CONNECTION=redis
+		'';
+	};
 
-  # --- PACKAGES ---
-  environment.systemPackages = with pkgs; [ 
-    git 
-    htop
-    nano
-    neofetch
-    ];
+	# users.users.nixos.openssh.authorizedKeys.keys = [
+	#   # Public Keys default nixos user
+	# "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJB9MG22hSHdYpwIWFRanUF88YvOYNcrV1zxAvv2RDJt taylort3450@syn-2600-6c5d-567f-3f2b-c338-35e0-ec14-df45.biz6.spectrum.com" 
+	# ];
 
-  # --- NIX SETTINGS ---
-  nix.settings.download-buffer-size = 524288000; # 500MiB
-  # Don't touch
-  system.stateVersion = "25.05"; 
+	users.users.root.openssh.authorizedKeys.keys =  [
+		# Public Keys for root user
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJB9MG22hSHdYpwIWFRanUF88YvOYNcrV1zxAvv2RDJt taylort3450@syn-2600-6c5d-567f-3f2b-c338-35e0-ec14-df45.biz6.spectrum.com" 
+	];
+
+	# --- PODMAN ---
+	virtualisation.podman = {
+		enable = true;
+		dockerCompat = true;
+		defaultNetwork.settings.dns_enabled = true;
+	};
+
+	# --- PACKAGES ---
+	environment.systemPackages = with pkgs; [ 
+		git 
+		htop
+		nano
+		neofetch
+		];
+
+	# --- NIX SETTINGS ---
+	nix.settings.download-buffer-size = 524288000; # 500MiB
+	# Don't touch
+	system.stateVersion = "25.05"; 
 }
